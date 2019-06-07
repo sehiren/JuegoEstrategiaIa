74bdfcd62041842edc9494c67b4ce145df3bcdab
Vicent Santamarta <vicent.santamarta@gmail.com>
2019-06-07 21:40:59 +0200

    Pequeños cambios en el selector random y el selector random

--- Juego Estrategia IA/Assets/Scripts/Artificial Inteligence/Behavior tree/NonDeterministicSelector.cs
+++ Juego Estrategia IA/Assets/Scripts/Artificial Inteligence/Behavior tree/NonDeterministicSelector.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new Random Selector", menuName = Task.MENU_NAME + Task.COMPOSITES + "Random Selector", order = 1)]
 public class NonDeterministicSelector : Task
 {
+    [SerializeField] private Task[] _children;
+
     public override TaskResult Run()
     {
-        throw new System.NotImplementedException();
+        Task[] shuffled = Task.Shuffle(_children);
+
+        foreach (Task child in shuffled)
+            if (child.Run() == TaskResult.Success)
+                return TaskResult.Success;
+
+        return TaskResult.Failure;
     }
 }
--- Juego Estrategia IA/Assets/Scripts/Artificial Inteligence/Behavior tree/NonDeterministicSequence.cs
+++ Juego Estrategia IA/Assets/Scripts/Artificial Inteligence/Behavior tree/NonDeterministicSequence.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Random Sequence", menuName = Task.MENU_NAME + Task.COMPOSITES + "Random Sequence", order = 1)]
 public class NonDeterministicSequence : Task
 {
+    [SerializeField] private Task[] _children;
+
     public override TaskResult Run()
     {
-        throw new System.NotImplementedException();
+        Task[] shuffled = Task.Shuffle(_children);
+
+        foreach (Task child in _children)
+            if (child.Run() == TaskResult.Failure)
+                return TaskResult.Failure;
+
+        return TaskResult.Success;
     }
 }
